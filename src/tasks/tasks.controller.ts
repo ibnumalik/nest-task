71ada1e0d2fbb3d0301e6c832bf0dcd7dbaac1f9
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-12-12 08:58:08 +0800

    Add route to update task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -35,4 +35,11 @@ export class TasksController {
     return this.tasksService.deleteTask(id);
   }
 
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -32,4 +32,9 @@ export class TasksService {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
 }
