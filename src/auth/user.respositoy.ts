8a32cab59efb781b054dda4bd94b4d23fb63633b
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-12-14 23:44:10 +0800

    Add validation check when creating new user

--- src/auth/user.respositoy.ts
+++ src/auth/user.respositoy.ts
@@ -1,15 +1,30 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-  async signUp(authCredentialsDto: AuthCredentialsDto) {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
     const user = new User();
     user.username = username;
     user.password = password;
-    user.save();
+
+    try {
+      await user.save();
+    } catch (error) {
+      const { code } = error;
+
+      if (Number(code) === 23505) {
+        throw new ConflictException('Username already taken');
+      }
+
+      throw new InternalServerErrorException();
+    }
   }
 }
